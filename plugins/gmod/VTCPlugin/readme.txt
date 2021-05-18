ac47c387a7b6e061661edfb5385e6e0d6ef21669
samdisk11 <samdisk11@github.com>
2021-05-18 11:32:27 -0500

    more games

---
+++ plugins/gmod/VTCPlugin/readme.txt
@@ -0,0 +1,3 @@
+this is a Addon for Garry's Mod, it does not need compiling (it is done by game automatically on startup)
+
+install this to /addons folder and configure /lua/vtc/sv_config.lua with your values.
---
+++ plugins/minecraft/VTCPluginMC/src/main/java/VTCPlugin.java
@@ -0,0 +1,10 @@
+// i have no idea what i am doing, this is my first MC plugin
+// and first time trying InteliJ, alone - i didnt even get basic training from
+// my friend scriptis
+// who helped me learn PHPStorm and DataGrip
+
+// pray this shit works
+import org.bukkit.plugin.java.JavaPlugin;
+public class VTCPlugin extends JavaPlugin {
+
+}
